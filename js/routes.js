ba77d1fc9410adbce982e5e1f1078cada98b71f7
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-11-04 15:04:08 +0200

    Routes update

--- js/routes.js
+++ js/routes.js
@@ -1,8 +1,10 @@
 import homePage from './pages/home-page.cmp.js';
 import aboutPage from './pages/about-page.cmp.js';
-import booksApp from './pages/books-app.cmp.js';
-import mailApp from './pages/mail-app.cmp.js';
-import keepApp from './pages/keep-app.cmp.js';
+import booksApp from './apps/Books/pages/books-app.cmp.js';
+import mailApp from './apps/Mail/pages/mail-app.cmp.js';
+import keepApp from './apps/Keep/pages/keep-app.cmp.js';
+import mailDetails from './apps/Mail/pages/mail-details.cmp.js';
+import mailCompose from './apps/Mail/pages/mail-compose.cmp.js';
 
 
 const myRoutes = [
@@ -16,7 +18,17 @@ const myRoutes = [
     },
     {
         path: '/mail',
-        component: mailApp
+        component: mailApp,
+        children: [
+            {
+                path: ':mailId',
+                component: mailDetails
+            },
+            {
+                path: 'mailCompose',
+                component: mailCompose
+            },
+        ]
     },
     {
         path: '/keep',
@@ -27,6 +39,8 @@ const myRoutes = [
       component: aboutPage
   },
 ]
+
+
 
 export const myRouter = new VueRouter({ routes: myRoutes })
 

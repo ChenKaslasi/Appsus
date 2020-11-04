3e9f3c30c1893ee88e6f0cf3717b60cc5d20fe13
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-11-04 16:03:48 +0200

    note service update

---
+++ js/apps/Mail/pages/mail-compose.cmp.js
@@ -0,0 +1,7 @@
+export default {
+  template: `
+  <section class="mail-app">
+    <h1>mail-compose</h1>
+  </section>
+  `
+}
---
+++ js/apps/Mail/pages/mail-details.cmp.js
@@ -0,0 +1,7 @@
+export default {
+  template: `
+  <section class="mail-details">
+    <h1>mail-details</h1>
+  </section>
+  `
+}

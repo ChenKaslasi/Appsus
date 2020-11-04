979c769adb5bd05f8c74fa7579f2d0cc9698dbe0
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-11-04 13:10:01 +0200

    commit 5

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -3,10 +3,10 @@ export default {
   <header class="app-header">
   <router-link to="/" ><h1>Appsus</h1></router-link>
     <nav>
-            <router-link to="/" >Home</router-link>|
-            <router-link to="/books" >MissBooks</router-link>|
-            <router-link to="/mail" >MisterEmail</router-link>|
-            <router-link to="/keep" >MissKeep</router-link>|
+            <router-link to="/" >Home</router-link>
+            <router-link to="/books" >MissBooks</router-link>
+            <router-link to="/mail" >MisterEmail</router-link>
+            <router-link to="/keep" >MissKeep</router-link>
             <router-link to="/about">About Us</router-link>
         </nav>        
 </header>

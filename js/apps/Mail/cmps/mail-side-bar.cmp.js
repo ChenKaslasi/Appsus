1686836d6f0b7a168b53c6461e64d98e19ac44e0
Elad Rahamim <eladr14@gmail.com>
2020-11-05 21:01:02 +0200

    after

--- js/apps/Mail/cmps/mail-side-bar.cmp.js
+++ js/apps/Mail/cmps/mail-side-bar.cmp.js
@@ -5,7 +5,7 @@ export default {
 <section class="side-bar">
     <button class="btn"><span><i class="fa fa-plus icon"></i></span>Compose</button>
     <ul>
-        <li><i class="icon fas fa-inbox"></i>Inbox</li>
+        <li><router-link to="/mail/inbox"><i class="icon fas fa-inbox"></i>Inbox</router-link></li>
         <li><i class="icon fas fa-star"></i>Starred</li>
         <li><i class="icon fas fa-paper-plane"></i>Sent</li>
         <li><i class="icon fas fa-sticky-note"></i>Drafts</li>

d65087c7b87b31dfa9f2aeb76071a042aa232057
Elad Rahamim <eladr14@gmail.com>
2020-11-07 22:53:03 +0200

    mail-res 0.0.2

--- js/apps/Mail/cmps/mail-side-bar.cmp.js
+++ js/apps/Mail/cmps/mail-side-bar.cmp.js
@@ -3,7 +3,7 @@ export default {
     name: 'side-bar',
     template: `
 <section class="side-bar">
-    <button class="btn" @click="emitOpenCompose"><span><i class="fa fa-plus icon"></i></span>Compose</button>
+    <button class="compose-btn" @click="emitOpenCompose"><span><i class="fa fa-plus icon"></i></span>Compose</button>
     <ul>
         <li class="btn-like"><router-link to="/mail/inbox"><i class="icon fas fa-inbox"></i>Inbox</router-link></li>
         <li class="btn-like"><router-link to="/mail/starred"><i class="icon fas fa-star"></i>Starred</router-link></li>
